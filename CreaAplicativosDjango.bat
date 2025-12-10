372d519933a7f29bb2f8a2abef5dac3d56734d2e
albertobernalf <alberto_bernalf@yahoo.com.co>
2025-12-10 14:05:11 -0500

    first commot

---
+++ CreaAplicativosDjango.bat
@@ -0,0 +1,35 @@
+cd c:\entornospython\pos7Particionado
+cd Scripts
+call activate
+cd ..
+pause
+django-admin startproject vulner
+cd vulner
+pause
+django-admin startapp Administracion
+django-admin startapp admisiones
+django-admin startapp autorizaciones
+django-admin startapp basicas
+django-admin startapp camara
+django-admin startapp cartera
+django-admin startapp cirugia
+django-admin startapp citasMedicas
+django-admin startapp clinico
+django-admin startapp contratacion
+django-admin startapp enfermeria
+django-admin startapp facturacion
+django-admin startapp farmacia
+django-admin startapp inventarios
+django-admin startapp mecanicoPacientes
+django-admin startapp planta
+django-admin startapp referencia
+django-admin startapp Reportes
+django-admin startapp rips
+django-admin startapp seguridad
+django-admin startapp sitios
+django-admin startapp tarifarios
+django-admin startapp terapeutico
+django-admin startapp triage
+django-admin startapp usuarios
+
+rem parte II Copiado de carpeta
